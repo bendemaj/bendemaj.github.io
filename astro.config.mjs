991193f9a2707f29e958954d1dff0d72694950a3
Ben Demaj <122387995+bendemaj@users.noreply.github.com>
2023-06-30 10:48:54 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { SITE_URL } from "./src/data/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
-  site: SITE_URL,
+  site: 'https://bendemaj.github.io',
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
